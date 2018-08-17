cc13c5cdac16c0c4742aadb4a6551171952c7901
nickmanks <nick.manks@gmail.com>
2018-08-17 13:53:01 +1000

    feat(connection): Initial implementation of basic connection
    Can connect 2 connectable object together and they will share corresponding inputs and outputs
    
    Closes #9

--- src/saij/core/Errors.js
+++ src/saij/core/Errors.js
@@ -2,6 +2,8 @@
  * @module saij/core/Errors
  */
 
+const message = 'Assertion failed. See https://skan-io.github.io/saij/module-saij_core_Errors for details.';
+
 /**
  * @enum {number}
  */
@@ -14,6 +16,16 @@ export default {
    */
   ASSERT_UNIQUE: {
     code: 3,
-    message: 'Assertion failed. See https://docs.saij.io/latest/errors/#3 for details.'
+    message: `[ERROR CODE 3] ${message}`
+  },
+  
+  /**
+   * Triggered when an connection is created without a string key.
+   * @type {module:saij/core/AssertionError~AssertionError}
+   * @api
+   */
+  ASSERT_CONNECTION_KEY: {
+    code: 4,
+    message: `[ERROR CODE 4] ${message}`
   }
 };
